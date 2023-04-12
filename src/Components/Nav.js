1ed20c0facf5d59cc69bf98619d259652b25d4fe
Jhonatan-Ordonez <jhoeduardonez@gmail.com>
2023-04-11 22:50:18 -0500

    Implemented nav bar, links and burguer menu in responsive

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,23 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "../Styles/nav.css";
 
-const nav = () => {
+//React icnos
+import { GrMenu } from "react-icons/gr";
+
+const Nav = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const handleMenu = () => {
+    setShowMenu(!showMenu);
+    
+  };
+
   return (
     <div className="main-nav">
-      <li>
-        <a href="/">1. About</a>
-      </li>
-      <li>
-        <a href="/">2. Experience</a>
-      </li>
-      <li>
-        <a href="/">3. Work</a>
-      </li>
-      <li>
-        <a href="/">4. Contact</a>
-      </li>
+      <div className={`nav-links ${showMenu && "active"}`}>
+        <a className="link" href="/">1. About</a>
+
+        <a className="link" href="/">2. Experience</a>
+
+        <a className="link" href="/">3. Work</a>
+
+        <a className="link" href="/">4. Contact</a>
+      </div>
+
+      <div className={`menu-icon ${showMenu && "active" }`} onClick={handleMenu} >
+        <GrMenu className="icon" />
+      </div>
     </div>
   );
 };
 
-export default nav;
+export default Nav;
