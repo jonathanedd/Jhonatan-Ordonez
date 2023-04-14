f911d839d71a942e88c7184090cfad48cde4bd4b
Jhonatan-Ordonez <jhoeduardonez@gmail.com>
2023-04-13 20:17:55 -0500

    Added text animation caida to home css

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,17 +5,17 @@ const Home = () => {
   return (
     <div className="home-main" id="#home">
       <div className="home-text">
-        <h5>Hi, my name is</h5>
-        <h1>Jhonatan Ordonez</h1>
-        <h2>I build things for the web</h2>
-        <span>
+        <h5 className="caida">Hi, my name is</h5>
+        <h1 className="caida">Jhonatan Ordonez</h1>
+        <h2 className="caida">I build things for the web.</h2>
+        <span className="caida">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat, nam?
           Eaque dicta voluptatum vero nemo dolor, accusamus maxime dignissimos
           nihil eius necessitatibus a, autem ipsam nesciunt nisi repellendus
           consequuntur itaque.
         </span>
 
-        <a className="message-me" href="/Home">
+        <a className="caida message-me" href="/Home">
           Message me
         </a>
       </div>
