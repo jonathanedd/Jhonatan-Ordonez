5206448f8dba78f97f8d848957b89bacb81003ec
Jhonatan-Ordonez <jhoeduardonez@gmail.com>
2023-05-02 16:54:47 -0500

    Starting using useState in order to change from Eng to Esp and viceversa

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,18 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "../Styles/home.css";
 
+const textEng = [
+  "I'm a Front-end software developer specializing in creating functional user interfaces.",
+  <br />,
+  " I love React and its tools such Redux and Hooks. Right now I´m focused on a new school sofware project called S.I.G.A which means ",
+  "Information system for academic management.",
+];
+
 const Home = () => {
+  const [language, setLanguage] = useState(true);
+  
+
+  const textEsp = [
+    "Soy desarrollador Front-end y me especializo en crear interfaces de usuario funcionales.",
+    <br />,
+    "Me encanta React y todas sus herramientas como Redux y los Hooks. Ahora mismo me encuentro trabajando en un proyecto de software para colegio, ",
+    "es un sistema de información para la gestión académica S.I.G.A.",
+  ];
+
+  const handleLanguage = () => {
+    setLanguage(!language)
+  };
+
   return (
     <div className="home-main" id="home">
       <div className="home-text">
         <h5 className="caida">Hi, my name is</h5>
         <h1 className="caida">Jhonatan Ordonez</h1>
         <h2 className="caida">I build things for the web.</h2>
-        <span className="caida">
+        {/* <span className="caida">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat, nam?
           Eaque dicta voluptatum vero nemo dolor, accusamus maxime dignissimos
           nihil eius necessitatibus a, autem ipsam nesciunt nisi repellendus
           consequuntur itaque.
+        </span> */}
+
+        <span className="caida">
+          {language ? textEng : textEsp} <button onClick={handleLanguage}>Change language</button>{" "}
         </span>
 
         <a className="caida message-me" href="/Home">
