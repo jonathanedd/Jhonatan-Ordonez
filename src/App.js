c09a9faebad7e84e9d2394518e7e2c960301d9be
Jhonatan-Ordonez <jhoeduardonez@gmail.com>
2023-07-09 21:34:57 -0500

    Added conditional render to Work component in App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
+import { useState } from "react";
 import "./App.css";
 import AboutMe from "./Components/AboutMe";
 import Home from "./Components/Home";
 import Nav from "./Components/Nav";
 import Work from "./Components/Work";
+import { FaDoorClosed } from "react-icons/fa";
 
 function App() {
+  const [showWork, setShowWork] = useState(false);
+  
   return (
     <div className="App">
       <Nav />
       <Home />
       <AboutMe />
-      <Work />
+
+      <button onClick={() => setShowWork(!showWork)}>Marvel App</button>
+      {
+        showWork === true && <Work/>
+      }
     </div>
   );
 }
